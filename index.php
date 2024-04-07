1ade50265d5231941611c6d19aee21214c967f6a
Kidmand <132107059+Kidmand@users.noreply.github.com>
2024-04-07 18:04:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
-<body>
-<?php
-$txt = "Hello world!";
-$x = 5;
-$y = 10.5;
-echo $txt;
-echo "<br>";
-echo $x;
-echo "<br>";
-echo $y;
-?>
-</body>
+  <body>
+    <?php
+      $txt = "Hello world!";
+      $x = 5;
+      $y = 10.5;
+      echo $txt;
+      echo "<br>";
+      echo $x;
+      echo "<br>";
+      echo $y;
+    ?>
+  </body>
 </html>
